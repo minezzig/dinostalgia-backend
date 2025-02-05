2cecb1742a92e5babe5d1eeaef8635e50a777088
Greg <gregminezzi@gmail.com>
2025-02-05 12:51:37 +0100

    submiting adds purchase item and list of ordered items to DB

--- orders/orders.controller.js
+++ orders/orders.controller.js
@@ -1,10 +1,13 @@
-import {addOrder} from './orders.service.js';
+import {addOrder, addOrderItems} from './orders.service.js';
 
 export const createOrder = async (req, res, next) => {
+  const {newOrder, cart } = req.body;
     try {
-      const newOrder = req.body;
-      const data = await addOrder(newOrder);
-      res.status(201).json(data);
+      // create order
+      const orderData = await addOrder(newOrder);
+      // insert ordered items to DB with ID
+      const orderItems = await addOrderItems(orderData.id, cart)
+      res.status(201).json("orderData", orderData, "orderItems", orderItems);
     } catch (error) {
       res.status(500).json({ error: error.message || 'Failed to create order' });
     }
--- orders/orders.service.js
+++ orders/orders.service.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 //connect to database
 const supabase = createClient(
@@ -7,17 +7,35 @@ const supabase = createClient(
 );
 
 export const addOrder = async (newOrder) => {
-  // add order to DB
-      // add purhcase to database with customer information
-      const { data, error} = await supabase
-      .from("orders")
-      .insert(newOrder)
-      .select()
-      .single();
+  // add purhcase to database with customer information
+  const { data, error } = await supabase
+    .from("orders")
+    .insert(newOrder)
+    .select()
+    .single();
 
-    // handle add purcahse to DB error
-    if (error) {
-      throw new Error("Error adding order to database");
-    }
+  // handle add purchase to DB error
+  if (error) {
+    throw new Error("Error adding order to database");
+  }
+  return data;
+};
+
+// add ordered items to DB using newly created orderId
+export const addOrderItems = async (orderId, cart) => {
+  const orderItems = cart.map((dino) => ({
+    order_id: orderId,
+    dino_id: dino.id,
+    name: dino.name,
+    price: dino.price,
+    quantity: dino.quantity,
+  }));
+
+  const { data, error } = await supabase
+    .from("order_items")
+    .insert(orderItems)
+    .select();
+
+  if (error) throw new Error("error adding ordered items to DB");
   return data;
 };
