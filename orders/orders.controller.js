83d7d2fd70fb63d45cd6a2f8205f0ac3aa93f493
Greg <gregminezzi@gmail.com>
2025-02-05 13:33:53 +0100

    add order validation

--- orders/orders.controller.js
+++ orders/orders.controller.js
@@ -2,6 +2,16 @@ import {addOrder, addOrderItems} from './orders.service.js';
 
 export const createOrder = async (req, res, next) => {
   const {newOrder, cart } = req.body;
+  const {first_name, last_name, email, address, total, user_id} = newOrder;
+
+  if (!first_name || !last_name || !email || !address || !total || !user_id) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+
+  if (isNaN(total)) {
+    return res.status(400).json({ error: "Total must be a valid number" });
+  }
+
     try {
       // create order
       const orderData = await addOrder(newOrder);
