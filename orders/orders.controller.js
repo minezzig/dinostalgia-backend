4c1bb490b89aa913054449db109437c5900c7d53
Greg <gregminezzi@gmail.com>
2025-02-05 12:18:21 +0100

    order post functioning

--- orders/orders.controller.js
+++ orders/orders.controller.js
@@ -1,9 +1,10 @@
 import {addOrder} from './orders.service.js';
 
-export const createOrder = async (req, res) => {
+export const createOrder = async (req, res, next) => {
     try {
-      const order = await addOrder();
-      res.json(order);
+      const newOrder = req.body;
+      const data = await addOrder(newOrder);
+      res.status(201).json(data);
     } catch (error) {
       res.status(500).json({ error: error.message || 'Failed to create order' });
     }
--- orders/orders.service.js
+++ orders/orders.service.js
@@ -6,7 +6,18 @@ const supabase = createClient(
   process.env.SUPABASE_ANON_KEY
 );
 
-export const addOrder = async () => {
+export const addOrder = async (newOrder) => {
   // add order to DB
+      // add purhcase to database with customer information
+      const { data, error} = await supabase
+      .from("orders")
+      .insert(newOrder)
+      .select()
+      .single();
+
+    // handle add purcahse to DB error
+    if (error) {
+      throw new Error("Error adding order to database");
+    }
   return data;
 };
